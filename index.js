84ebe4ef459db5eee0d3f88022a7ed01913bbf02
maklad42 <maklad42+git@gmail.com>
2022-01-10 20:49:13 +0900

    Adds multiple URLs. Need to add more URLs and get them saved to a JSON object to display as a table of chart. 220110

--- index.js
+++ index.js
@@ -1,39 +1,58 @@
 const puppeteer = require('puppeteer');
-const ch = require('cheerio');
 const CronJob = require('cron').CronJob;
 const nodemailer = require('nodemailer');
 
 // const url =
 //   'https://www.amazon.co.jp/%E3%82%BD%E3%83%8B%E3%83%BC-%E3%83%AF%E3%82%A4%E3%83%A4%E3%83%AC%E3%82%B9%E3%83%8E%E3%82%A4%E3%82%BA%E3%82%AD%E3%83%A3%E3%83%B3%E3%82%BB%E3%83%AA%E3%83%B3%E3%82%B0%E3%83%98%E3%83%83%E3%83%89%E3%83%9B%E3%83%B3-WH-1000XM4-Bluetooth-%E6%9C%80%E5%A4%A730%E6%99%82%E9%96%93%E9%80%A3%E7%B6%9A%E5%86%8D%E7%94%9F/dp/B08F2866Q3?th=1';
 
-const url = 'https://www.amazon.co.jp/hz/wishlist/ls/2O6CSNTML0GMQ';
+// List of individual book page URLs
+const urls = [
+  'https://www.amazon.co.jp/dp/B005I54882/?coliid=I3R3IRHJWQB3M6&colid=2O6CSNTML0GMQ&psc=0&ref_=gv_ov_lig_pi_dp',
+  'https://www.amazon.co.jp/dp/B005PM3YII/?coliid=I3I66D29TJWJN&colid=2O6CSNTML0GMQ&psc=0&ref_=gv_ov_lig_pi_dp',
+  'https://www.amazon.co.jp/dp/B003XNTTYY/?coliid=I1MWYPTIE1ARQ6&colid=2O6CSNTML0GMQ&psc=0&ref_=gv_ov_lig_pi_dp',
+  'https://www.amazon.co.jp/dp/B091SMJLP6/?coliid=IX1QLIVKFTRSF&colid=2O6CSNTML0GMQ&psc=0&ref_=gv_ov_lig_pi_dp',
+];
 
-async function configureBrowser() {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto(url);
-  return page;
+let books = [];
+
+urls.forEach((url) => {
+  let details = getBooksDetails(url);
+  books.push(details);
+});
+
+console.log(books);
+
+async function getBooksDetails(url) {
+  let browser = await puppeteer.launch();
+  let page = await browser.newPage();
+  await page.goto(url, { waitUntil: 'networkidle0' });
+
+  // // Book wishlist
+  // let data = await page.evaluate(() => {
+  //   let html = document.querySelectorAll('li.g-item-sortable');
+  //   const books = Array.from(html);
+
+  //   return books;
+
+  // Individual book page (test)
+  let data = await page.evaluate(() => {
+    let title = document.getElementById('productTitle').innerText;
+    let price = document
+      .querySelector('li.swatchElement.selected span.a-color-price')
+      .innerText.replace(/[^0-9]/g, '');
+
+    // let price = 0;
+
+    return {
+      title,
+      price,
+    };
+  });
+
+  console.log(data);
+
+  // let html = await page.evaluate(() => document.body.innerHTML);
+  // console.log(html);
+
+  await browser.close();
 }
-
-// li#item_I3R3IRHJWQB3M6
-// span.a-text-price span.a-offscreen
-
-async function checkPrice(page) {
-  await page.reload();
-  let html = await page.evaluate(() => document.body.innerHTML);
-  console.log(html);
-
-  // const $ = ch.load(html);
-
-  // $('#item_I3R3IRHJWQB3M6', html).each(function () {
-  //   let currPrice = $(this).innerHTML();
-  //   console.log(currPrice);
-  // });
-}
-
-async function monitor() {
-  let page = await configureBrowser();
-  await checkPrice(page);
-}
-
-monitor();
---
+++ wishlist.js
@@ -0,0 +1,87 @@
+const puppeteer = require('puppeteer');
+// const devices = require('puppeteer/DeviceDescriptors');
+// const iPhone = devices['iPhone 8'];
+// const urlbase = 'https://www.amazon.co.jp/hz/wishlist/ls/';
+const url = 'https://www.amazon.co.jp/hz/wishlist/ls/2O6CSNTML0GMQ';
+
+// async function getProductInfo(wishListId) {
+async function getProductInfo() {
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+  });
+  try {
+    const page = await initPage(browser);
+    // 欲しいものリストのURLを開く
+    // await page.goto(urlbase + wishListId);
+    await page.goto(url);
+    // スクレイピング(中身は後述)
+    const list = await scrapePage(page);
+
+    console.log(list);
+
+    return list;
+  } catch (err) {
+    console.error(err);
+    throw err;
+  } finally {
+    browser.close();
+  }
+}
+
+async function initPage(browser) {
+  const page = await browser.newPage();
+  // await page.emulate(iPhone);
+  return page;
+}
+
+async function scrapePage(page) {
+  return await page.evaluate(async () => {
+    // スクロールでの移動距離と待機間隔ms
+    const distance = 500;
+    const delay = 100;
+    // リストの末尾が検知されない限りループする
+    while (!document.querySelector('#endOfListMarker')) {
+      // 500pxずつスクロール移動して、100ミリ秒待機する
+      document.scrollingElement.scrollBy(0, distance);
+      await new Promise((resolve) => {
+        setTimeout(resolve, delay);
+      });
+    }
+
+    // 全ての商品の表示が終わったらスクレイピングを実施
+    const itemList = [{ first: 'Test' }];
+    // 商品の情報のBOX単位でデータを切り出す
+    // [...document.querySelectorAll('a[href^="/dp/"].a-touch-link')].forEach(
+    [...document.querySelectorAll('.g-item-list')].forEach((el) => {
+      // const productID = el
+      //   .getAttribute('href')
+      //   .split('/?coliid')[0]
+      //   .replace('/dp/', '');
+
+      // title
+      // const title = el.querySelector('[id^="item_title_"]').textContent;
+      const title = el.querySelector('a[href^="/dp/"]').textContent;
+
+      // price
+      let price = -1;
+      // const priceEle = el.querySelector('[id^="itemPrice_"] > span');
+
+      // const priceEle = el.querySelector('span.a-price-whole');
+      // if (priceEle && priceEle.textContent) {
+      //   // price = Number(priceEle.textContent.replace('￥', '').replace(',', ''));
+      //   price = priceEle.textContent;
+      // }
+
+      itemList.push({
+        price: price,
+        title: 'Help',
+        // productID: productID,
+      });
+      debugger;
+    });
+    return itemList;
+  });
+}
+
+getProductInfo();
