58692ae5fdf6304ab83041ae3e2cff5ce4c60c73
maklad42 <maklad42+git@gmail.com>
2022-01-15 21:52:07 +0900

    Updates the loop to get each books title and price. Now async running in parallel. Adds more books URLs. Adds sample JSON file for the currently listed books. 220115

--- index.js
+++ index.js
@@ -3,53 +3,79 @@ const CronJob = require('cron').CronJob;
 const nodemailer = require('nodemailer');
 
 // ToDo: set up cron job
-// ToDo: save result to a
+// ToDo: save result to a json file
 
-// List of individual book page URLs
-const urls = [
-  'https://www.amazon.co.jp/dp/B005I54882/?coliid=I3R3IRHJWQB3M6&colid=2O6CSNTML0GMQ&psc=0&ref_=gv_ov_lig_pi_dp',
-  'https://www.amazon.co.jp/dp/B005PM3YII/?coliid=I3I66D29TJWJN&colid=2O6CSNTML0GMQ&psc=0&ref_=gv_ov_lig_pi_dp',
-  'https://www.amazon.co.jp/dp/B003XNTTYY/?coliid=I1MWYPTIE1ARQ6&colid=2O6CSNTML0GMQ&psc=0&ref_=gv_ov_lig_pi_dp',
-  'https://www.amazon.co.jp/dp/B091SMJLP6/?coliid=IX1QLIVKFTRSF&colid=2O6CSNTML0GMQ&psc=0&ref_=gv_ov_lig_pi_dp',
-  'https://www.amazon.co.jp/dp/B0064CZ1XE/?coliid=I1J5MQC2L4UZK0&colid=2O6CSNTML0GMQ&psc=0&ref_=gv_ov_lig_pi_dp',
-  'https://www.amazon.co.jp/dp/B00HFG1QSM/?coliid=I2JVETWD0KC7K9&colid=2O6CSNTML0GMQ&psc=0&ref_=gv_ov_lig_pi_dp',
-  'https://www.amazon.co.jp/dp/B016XPPJ4W/?coliid=ISPVJ0112246S&colid=2O6CSNTML0GMQ&psc=0&ref_=gv_ov_lig_pi_dp',
-  'https://www.amazon.co.jp/dp/B00HFG1QSC/?coliid=I194F28K9273LK&colid=2O6CSNTML0GMQ&psc=0&ref_=gv_ov_lig_pi_dp',
-  'https://www.amazon.co.jp/dp/B004X1V1CS/?coliid=I29W8WHWU6KW7T&colid=2O6CSNTML0GMQ&psc=0&ref_=gv_ov_lig_pi_dp',
-  'https://www.amazon.co.jp/dp/B08Z3D14TC/?coliid=I1ULW07M0ZUMWB&colid=2O6CSNTML0GMQ&psc=0&ref_=gv_ov_lig_pi_dp',
-  'https://www.amazon.co.jp/dp/B019CGY2ZG/?coliid=I28VFSP0Q8JAKU&colid=2O6CSNTML0GMQ&psc=0&ref_=gv_ov_lig_pi_dp',
-  'https://www.amazon.co.jp/dp/B08QN55X7B/?coliid=IUAKP5KE521G9&colid=2O6CSNTML0GMQ&psc=0&ref_=gv_ov_lig_pi_dp',
-];
+// ToDo: create file to display the price changes
 
-async function createBookList(urls) {
-  let books = [];
-  const promises = [];
-  for (const url of urls) {
-    promises.push(getBookDetails(url));
-  }
-  books = await Promise.all(promises);
+(async () => {
+  const browser = await puppeteer.launch();
+  const baseURL = 'https://www.amazon.co.jp/dp/';
+  const urls = [
+    'B005I54882',
+    'B005PM3YII',
+    'B003XNTTYY',
+    'B091SMJLP6',
+    'B0064CZ1XE',
+    'B00HFG1QSM',
+    'B016XPPJ4W',
+    'B00HFG1QSC',
+    'B004X1V1CS',
+    'B08Z3D14TC',
+    'B019CGY2ZG',
+    'B08QN55X7B',
+    'B09LMTQK6P',
+    'B07Q85C6VX',
+    'B00KVI76ZS',
+    'B086V2NKW6',
+    'B014POFNPI',
+    'B07H33TBB2',
+    'B07DNX4TT5',
+    'B002VBV1S6',
+    'B00N1RM8CM',
+    'B07BD2B1CT',
+    'B0776L9W8C',
+    'B000FC0XV4',
+    'B003P2WO5E',
+    'B01MYEM8SZ',
+    'B004J4XGN6',
+    'B00316UMS0',
+    '0143129252',
+    'B07637KVXL',
+    'B07DBRBP7G',
+    'B00FOVTOMA',
+    'B005J3IEZQ',
+    'B006I1AE92',
+    'B00555X8OA',
+    'B002Q6XUE4',
+    'B00DXKJ2DA',
+    'B07M82PNSX',
+    'B01HSMRWNU',
+    'B00AFPVOTG',
+  ];
+  const len = urls.length;
+
+  const books = (
+    await Promise.allSettled(
+      [...Array(len)].map(async (_, i) => {
+        const page = await browser.newPage();
+        await page.goto(`${baseURL}/${urls[i]}`);
+        let data = await page.evaluate(() => {
+          let title = document.getElementById('productTitle').innerText;
+          let price = document
+            .querySelector('li.swatchElement.selected span.a-color-price')
+            .innerText.replace(/[^0-9]/g, '');
+
+          return {
+            title,
+            price,
+          };
+        });
+        return data;
+      })
+    )
+  )
+    .filter((e) => e.status === 'fulfilled')
+    .map((e) => e.value);
   console.log(books);
-  return books;
-}
-
-async function getBookDetails(url) {
-  let browser = await puppeteer.launch();
-  let page = await browser.newPage();
-  await page.goto(url, { waitUntil: 'networkidle0' });
-
-  let data = await page.evaluate(() => {
-    let title = document.getElementById('productTitle').innerText;
-    let price = document
-      .querySelector('li.swatchElement.selected span.a-color-price')
-      .innerText.replace(/[^0-9]/g, '');
-
-    return {
-      title,
-      price,
-    };
-  });
-  browser.close();
-  return data;
-}
-
-createBookList(urls);
+  await browser.close();
+})();
